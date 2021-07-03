7e5345997b5ed6129b180da8280938d9dd0e5f2e
Septimiu Bloj <septimiubloj@gmail.com>
2021-07-03 13:50:06 +0300

    Add constructor method
    - class components also gives access to the constructor function, which
    in turn, gives access to the component's state, the main benefit of
    converting our functional component into a class component
    - it also gives access to the setState method, which is the only way to
    modify the component's state object, without breaking the
    unidirectional data flow concept

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 // starting from React 17, it's not necssary to import React to use JSX
-import { Component } from 'react' // import destructured Component from react
-import './App.css';
+import { Component } from "react"; // import destructured Component from react
+import "./App.css";
 
 // Convert App from functional to class component:
 // - this allows more functionality vs. just returning HTML
 class App extends Component {
+  // - the class component also has a constructor method
+  constructor() {
+    super();
+
+    // - which gives access to the component's state
+    // - also gives access to setState method to modify this.state object
+    this.state = {
+      string: "Hello, world!",
+    };
+  }
+
   // - access to the render method
   render() {
     // - which has a return
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>{this.state.string}</p>
+          <button onClick={() => this.setState({ string: "Hello, universe!" })}>
+            Change text
+          </button>
         </header>
       </div>
     );
