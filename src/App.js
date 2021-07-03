8e1e4ce2c246917c1426ff69bf2e3d4eceb03556
Septimiu Bloj <septimiubloj@gmail.com>
2021-07-03 13:15:54 +0300

    Convert to class component
    - allow more functionality, get access to render method

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
+// starting from React 17, it's not necssary to import React to use JSX
+import { Component } from 'react' // import destructured Component from react
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Convert App from functional to class component:
+// - this allows more functionality vs. just returning HTML
+class App extends Component {
+  // - access to the render method
+  render() {
+    // - which has a return
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
