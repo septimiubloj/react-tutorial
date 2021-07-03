349918cf8534eefd5e68f3f2925d74b8538fbf45
Septimiu Bloj <septimiubloj@gmail.com>
2021-07-03 13:31:50 +0300

    Declutter template

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 // starting from React 17, it's not necssary to import React to use JSX
 import { Component } from 'react' // import destructured Component from react
-import logo from './logo.svg';
 import './App.css';
 
 // Convert App from functional to class component:
@@ -12,18 +11,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
